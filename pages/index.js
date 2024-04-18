a5c1df36377e95ce130fd836179067403f3cd4d8
Stellan Wetzig <stellanwetzig@yahoo.de>
2024-04-18 21:44:07 +0200

    fix elint error

--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,9 @@ export default function HomePage() {
   ];
 
   const items = [
-    <div>Container 1</div>,
-    <div>Container 2</div>,
-    <div>Container 3</div>,
-    <div>Container 4</div>,
-    <div>Container 5</div>,
-    <div>Container 6</div>,
-    <div>Container 7</div>,
-    <div>Container 8</div>,
-    <div>Container 9</div>,
+    <div key={1}>Container 1</div>,
+    <div key={2}>Container 2</div>,
+    <div key={3}>Container 3</div>,
   ];
 
   return (
