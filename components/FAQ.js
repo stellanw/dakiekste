392fd300d60f03300d9bfd984bbe983631f7cb07
Stellan Wetzig <stellanwetzig@yahoo.de>
2025-01-02 21:56:31 +0100

    modified and complete styling according to design template in figma

--- components/FAQ.js
+++ components/FAQ.js
@@ -26,14 +26,18 @@ export default function FAQ({ faqData }) {
             <StyledGroup>
               <StyledGroupChild1>
                 <StyledCaption>{faq.caption}</StyledCaption>
-                <StyledQuestion>{faq.question}</StyledQuestion>
               </StyledGroupChild1>
-              <StyledPlusIcon
-                className={isOpen[index] ? "rotate" : ""}
-                onClick={() => toggleOverlay(index)}
-              />
+              <StyledGroupChild2 key={index} className={isOpen[index] ? "open" : ""}>
+                <StyledQuestionIconGroup>
+                  <StyledQuestion>{faq.question}</StyledQuestion>
+                  <StyledPlusIcon
+                    className={isOpen[index] ? "rotate" : ""}
+                    onClick={() => toggleOverlay(index)}
+                  />
+                </StyledQuestionIconGroup>
+                {isOpen[index] && <StyledOverlayParagraph>{faq.answer}</StyledOverlayParagraph>}
+              </StyledGroupChild2>
             </StyledGroup>
-            {isOpen[index] && <StyledOverlayParagraph>{faq.answer}</StyledOverlayParagraph>}
           </StyledLiGroup>
         ))}
       </StyledUl>
@@ -66,7 +70,6 @@ const StyledFAQContainer = styled.section`
   width: 100%;
   margin: 0;
   padding: ${theme.spacing.l} 0;
-
   background-color: ${theme.color.dark};
   color: ${theme.color.beige};
   margin-bottom: -0.5rem;
@@ -77,7 +80,7 @@ const StyledHeadlineContainer = styled.div`
   flex-direction: column;
   align-items: start;
   position: relative;
-  margin: 0rem 2rem 2rem 0rem;
+  margin: 0 0 ${theme.spacing.xl} 0;
   width: 100%;
 
   h5 {
@@ -92,7 +95,7 @@ const StyledHeadlineContainer = styled.div`
   }
 `;
 
-const StyledCaption = styled.h4`
+const StyledCaption = styled.h5`
   text-transform: uppercase;
   font-weight: ${theme.fontWeight.light};
   min-width: 180px;
@@ -102,7 +105,6 @@ const StyledCaption = styled.h4`
 const StyledQuestion = styled.span`
   font-weight: ${theme.fontWeight.bold};
   text-transform: uppercase;
-  margin-left: 1rem;
 `;
 
 const StyledUl = styled.ul`
@@ -115,34 +117,42 @@ const StyledUl = styled.ul`
 const StyledLiGroup = styled.li`
   display: flex;
   flex-direction: column;
-  padding: ${theme.spacing.xl};
-  transition: background-color 0.3s ease, color 0.3s ease;
   border-top: 1px solid ${theme.color.beige};
   border-bottom: 1px solid ${theme.color.beige};
+`;
+
+const StyledGroup = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const StyledGroupChild1 = styled.div`
+  padding: ${theme.spacing.l} 0 ${theme.spacing.l} ${theme.spacing.l};
+`;
+
+const StyledGroupChild2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: ${theme.spacing.l} ${theme.spacing.l} ${theme.spacing.l} ${theme.spacing.l};
+  transition: background-color 0.3s ease, color 0.3s ease;
   &.open {
     background-color: ${theme.color.green};
     color: ${theme.color.dark};
   }
 `;
 
-const StyledGroup = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-`;
-
-const StyledGroupChild1 = styled.div`
+const StyledQuestionIconGroup = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space;
-  gap: ${theme.spacing.xl};
+  justify-content: space-between;
+  width: 100%;
 `;
 
 const StyledOverlayParagraph = styled.p`
   font-size: ${theme.fontSizes.s};
-  font-weight: 200;
-  padding: ${theme.spacing.l} 0 0 0;
+  font-weight: ${theme.fontWeight.light};
+  padding: ${theme.spacing.m} 0 0 0;
   animation-name: slide-animation;
   animation-duration: 0.5s;
   transition: opacity 0.3s ease;
